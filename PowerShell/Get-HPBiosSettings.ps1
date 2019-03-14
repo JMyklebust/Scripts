0ef8d613a1760c369ce633ad9729e098889a9da2
JMyklebust <jensmyklebust@outlook.com>
2019-03-14 14:17:23 +0100

    Get-HPBioSettings - Added BIOS version to output header - Changed order of session close to end immediatly after getting data. - Aligned the headers to make them easier to read.

--- PowerShell/Get-HPBiosSettings.ps1
+++ PowerShell/Get-HPBiosSettings.ps1
@@ -21,6 +21,11 @@ catch {
 $ComputerInfo = Get-CimInstance -ClassName CIM_ComputerSystem -CimSession $Session
 #Gather HP BIOS info from CIM class
 $HPBIOS = Get-CimInstance -Namespace root/HP/InstrumentedBIOS -ClassName HPBIOS_BIOSEnumeration -CimSession $Session
+#Get BIOS Verison
+$BIOSVersion = (Get-CimInstance -ClassName CIM_BIOSElement -CimSession $Session).SMBIOSBIOSVersion 
+#End session connection as data has been collected
+Remove-CimSession $Session
+
 
 #Output file is a csv with the hostname and computer model, all spaces replaced with underscore
 $OutFile = "$LogFolder\$($ComputerInfo.Name)_$($ComputerInfo.Model).csv" -replace ' ','_'
@@ -32,8 +37,5 @@ $HPBIOS = $HPBIOS | Where-Object -FilterScript {$_.name -match '\S'} | Sort-Obje
 $HPBIOS | Select-Object Path,Name,CurrentValue,@{n="PossibleValues";e={($_ | Select -ExpandProperty PossibleValues) -join ','}} | Export-Csv -Path $OutFile -NoTypeInformation -Encoding ASCII
 
 #Add computer info on top of output file
-@("#Hostname: $($ComputerInfo.Name)", "#Model: $($ComputerInfo.Model)","#Date: $(Get-Date -Format s)") +  (Get-Content $OutFile -Encoding Ascii) | Set-Content $OutFile -Encoding Ascii
-
-#Clean up session connection
-Remove-CimSession $Computer
+@("#Date:      $(Get-Date -Format s)","#Hostname:  $($ComputerInfo.Name)", "#Model:     $($ComputerInfo.Model)","#BIOS Ver.: $BIOSVersion","") +  (Get-Content $OutFile -Encoding Ascii) | Set-Content $OutFile -Encoding Ascii
 
